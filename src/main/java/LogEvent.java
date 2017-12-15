aff961f9d3e602778bdaa4d0bc0b6033be3f2369
Varsha <bhanushali.v@husky.neu.edu>
2017-12-15 16:48:26 -0500

    event logs

--- src/main/java/LogEvent.java
+++ src/main/java/LogEvent.java
@@ -22,7 +22,7 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
   private Regions REGION = Regions.US_EAST_1;
   static final String FROM = "noreply@csye6225-fall2017-bhanushaliv.me";
   static final String SUBJECT = "Reset Password Link";
-  static  String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+  static  String HTMLBODY = "<h1>Csye 6225 Password reset link</h1>"
           + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
           + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
           + "AWS SDK for Java</a>";
@@ -40,42 +40,79 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
     timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
     context.getLogger().log("Invocation completed: " + timeStamp);
 
-    //Execution
-    String username = request.getRecords().get(0).getSNS().getMessage();
-    String token = UUID.randomUUID().toString();
-    this.initDynamoDbClient();
-    this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
-            .putItem(
-                    new PutItemSpec().withItem(new Item()
-                            .withString("id", username)
-                            .withString("token", token)
-                            .withInt("TTL", 1800)));
-    textBody = "https://csye6225-fall2017.com/reset?email="+username+"&token="+token;
-    try {
-      AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
-              .withRegion(Regions.US_EAST_1).build();
+      String username = request.getRecords().get(0).getSNS().getMessage();
+      String token = UUID.randomUUID().toString();
+      this.initDynamoDbClient();
+      Item existUser = this.dynamoDb.getTable(DYNAMODB_TABLE_NAME).getItem("id", username);
 
-     String BODY = HTMLBODY+textBody;
-      SendEmailRequest emailRequest = new SendEmailRequest()
-              .withDestination(
-                      new Destination().withToAddresses(username))
-              .withMessage(new Message()
-                      .withBody(new Body()
-                              .withHtml(new Content()
-                                      .withCharset("UTF-8").withData(BODY))
-                              .withText(new Content()
-                                      .withCharset("UTF-8").withData(textBody)))
-                      .withSubject(new Content()
-                              .withCharset("UTF-8").withData(SUBJECT)))
-              .withSource(FROM);
-      client.sendEmail(emailRequest);
+      if (existUser == null) {
+          this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                  .putItem(
+                          new PutItemSpec().withItem(new Item()
+                                  .withString("id", username)
+                                  .withString("token", token)
+                                  .withLong("TTL", System.currentTimeMillis() / 1000L + 60)));
+          textBody = "https://csye6225-fall2017.com/reset?email=" + username + "&token=" + token;
+          try {
+              AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+                      .withRegion(Regions.US_EAST_1).build();
+
+              String body = HTMLBODY + "<p> "+textBody+"</p>";
+              SendEmailRequest emailRequest = new SendEmailRequest()
+                      .withDestination(
+                              new Destination().withToAddresses(username))
+                      .withMessage(new Message()
+                              .withBody(new Body()
+                                      .withHtml(new Content()
+                                              .withCharset("UTF-8").withData(body))
+                                      .withText(new Content()
+                                              .withCharset("UTF-8").withData(textBody)))
+                              .withSubject(new Content()
+                                      .withCharset("UTF-8").withData(SUBJECT)))
+                      .withSource(FROM);
+              client.sendEmail(emailRequest);
 
 
-        System.out.println("Email sent successfully!");
-    } catch (Exception ex) {
-      System.out.println("The email was not sent. Error message: "
-              + ex.getMessage());
-    }
+              System.out.println("Email sent successfully!");
+          } catch (Exception ex) {
+              System.out.println("The email was not sent. Error message: "
+                      + ex.getMessage());
+          }
+      } else {
+          this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                  .putItem(
+                          new PutItemSpec().withItem(new Item()
+                                  .withString("id", username)
+                                  .withString("token", token)
+                                  .withInt("TTL", 1800)));
+
+          try {
+              AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+                      .withRegion(Regions.US_EAST_1).build();
+
+              String body = HTMLBODY + "<p>Password reset link already sent</p>";
+
+              SendEmailRequest emailRequest = new SendEmailRequest()
+                      .withDestination(
+                              new Destination().withToAddresses(username))
+                      .withMessage(new Message()
+                              .withBody(new Body()
+                                      .withHtml(new Content()
+                                              .withCharset("UTF-8").withData(body))
+                                      .withText(new Content()
+                                              .withCharset("UTF-8").withData(textBody)))
+                              .withSubject(new Content()
+                                      .withCharset("UTF-8").withData(SUBJECT)))
+                      .withSource(FROM);
+              client.sendEmail(emailRequest);
+
+
+              System.out.println("Email sent successfully!");
+          } catch (Exception ex) {
+              System.out.println("The email was not sent. Error message: "
+                      + ex.getMessage());
+          }
+      }
     return null;
   }
 
